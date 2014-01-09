1b24043f054d8814e5ce237dbcf4b1c2a1f365e4
zoeoberon <joan@zoeoberon.com>
2014-01-09 21:00:50 +0000

    tweak development environment settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ RailsStarter::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -26,6 +26,9 @@ RailsStarter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Raise an error if unpermitted parameter in controller
+  config.action_controller.action_on_unpermitted_parameters = :raise
 
   config.action_mailer.default_url_options = { :host => PRIVATE_DATA['url_host'] }
   config.action_mailer.perform_deliveries = true
