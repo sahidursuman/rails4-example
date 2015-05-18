62b962ce513d5df6c19c809f0e21b5c0a908acdd
zoeoberon <joan@zoeoberon.com>
2015-05-18 19:22:37 +0000

    annonate models

--- spec/models/devise_usage_log_spec.rb
+++ spec/models/devise_usage_log_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: devise_usage_logs
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  user_ip    :string(255)
+#  role       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  username   :string(255)
+#  action     :string(255)
+#
+
 require 'spec_helper'
 
 describe DeviseUsageLog do
