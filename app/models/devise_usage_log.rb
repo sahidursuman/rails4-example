9ada46c6268f8ae01a16166f4dfd24e634734a73
zoeoberon <joan@zoeoberon.com>
2015-05-18 19:21:48 +0000

    annonate models

--- app/models/devise_usage_log.rb
+++ app/models/devise_usage_log.rb
@@ -1,10 +1,24 @@
+# == Schema Information
+#
+# Table name: devise_usage_logs
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  user_ip    :string(255)
+#  role       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  username   :string(255)
+#  action     :string(255)
+#
+
 class DeviseUsageLog < ActiveRecord::Base
 
   classy_enum_attr :action, enum: 'DeviseAction', allow_nil: true
 
   def self.fetch_usage_report_entries(num_days = 10)
     # rubocop:disable MultilineOperationIndentation
-    DeviseUsageLog.where('created_at > ?', DateTime.now - num_days.days)
+    DeviseUsageLog.where('created_at > ?', DateTime.zone.now - num_days.days)
                   .order('devise_usage_logs.created_at desc')
                   .order('user_id')
     # rubocop:enable all
