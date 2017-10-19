dc5a909ed61dedbb361a4f7b17e22790af79e394
jehughes <jehughes@gmail.com>
2017-10-19 14:47:36 -0400

    working on travis CI

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,7 @@ class User < ActiveRecord::Base
 
   validates :username, uniqueness: true, length: {within: 4..12}
 
-  ROLES = {'guest' => 1, 'user' => 2, 'manager' => 3, 'admin' => 4}.freeze
+  ROLES = {'guest' => 1, 'user' => 2, 'manager' => 3, 'admin' => 4}
 
   def roles
     ROLES
