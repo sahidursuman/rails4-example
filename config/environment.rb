650a6cb2db9467645cfc7fe6392d978c0d9a23fa
jehughes <jehughes@gmail.com>
2017-10-19 15:12:02 -0400

    working on travis CI

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-VERSION = '1.0.0'
+VERSION = '1.0.0'.freeze
 
 # Initialize the Rails application.
 Rails.application.initialize!
